ff7323465a60a951cae724c1f2a82ef1b1603ecb
KathiaSegovia <kathiasegovia@gmail.com>
2021-01-09 00:25:50 -0700

    first commit

---
+++ src/components/button/__test__/button.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from './../button';
+import { isTSAnyKeyword } from '@babel/types'
+
+import { render, cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect"; 
+import renderer from 'react-test-renderer';
+
+afterEach(cleanup);
+
+  it ("renders without crashing",()=>{
+    const div = document.createElement("div");
+    ReactDOM.render(<Button></Button>, div)
+  }); 
+
+  it ("renders button correctly",()=>{
+    const {getByTestId} = render(<Button label = "BUY NOW" > </Button>)
+    expect (getByTestId('button')).toHaveTextContent("BUY NOW")
+  });
+
+  it ("renders button correctly",()=>{
+    const {getByTestId} = render(<Button label = "SAVE" > </Button>)
+    expect (getByTestId('button')).toHaveTextContent("SAVE")
+  });
+
+  it ("matches snapshot",() =>{
+    const tree = renderer.create(<Button label = "SAVE"></Button>).toJSON()
+    expect(tree).toMatchSnapshot();
+  })
+
