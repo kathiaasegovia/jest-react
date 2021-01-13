2a8e83122dc681236a79c7590477beab1ca5779f
KathiaSegovia <kathiasegovia@gmail.com>
2021-01-12 23:50:51 -0700

    Add navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './App.css';
 import Button from './components/button/button';
 
+import { Navbar,Nav, NavDropdown} from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Jest Unit Testing</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
       <header className="App-header">
         <Button label="BUY NOW"></Button>
