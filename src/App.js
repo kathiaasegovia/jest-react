a814519c90e9cab28d56445dcad910c14e17746d
KathiaSegovia <kathiasegovia@gmail.com>
2021-01-14 20:10:34 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
 import React from 'react';
 import './App.css';
-import Button from './components/button/button';
-import {Navbar, Nav, NavDropdown, Carousel} from 'react-bootstrap';
+import Button from     './components/button/button';
+import { Navbar2 } from'./components/navbar/navbar';
+import { Slider } from './components/slider/slider';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Jest-Enzyme</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
-
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://source.unsplash.com/7jQh3EiS8Bs"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://source.unsplash.com/7jQh3EiS8Bs"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://source.unsplash.com/7jQh3EiS8Bs"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-
+      <Navbar2 />
+      <Slider />
       <header className="App-header">
         <Button label="BUY NOW"></Button>
       </header>
